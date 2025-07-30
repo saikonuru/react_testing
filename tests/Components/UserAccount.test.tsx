b80002b9cb698d1ecd17f3a015f59bd2f524f0a7
saikonuru <sairam.konuru@gmail.com>
2025-07-30 22:08:00 +0530

    Test cases for USer Account

---
+++ tests/Components/UserAccount.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+
+import UserAccount from "../../src/components/UserAccount";
+import type { User } from "../../src/entities";
+
+describe("User Account", () => {
+  const user: User = {
+    id: 1,
+    name: "Sairam",
+    isAdmin: true,
+  };
+
+  it("shoud render user name", () => {
+    render(<UserAccount user={user} />);
+
+    expect(screen.getByText(user.name)).toBeInTheDocument();
+  });
+  it("shoud display enable edit button if user is admin", () => {
+    render(<UserAccount user={user} />);
+
+    const button = screen.getByRole("button"); // getByRole
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/edit/i);
+  });
+
+  it("shoud not display edit button if user is not admin", () => {
+    user.isAdmin = false;
+    render(<UserAccount user={user} />);
+    const button = screen.queryByRole("button"); //queryByRole
+    expect(button).not.toBeInTheDocument();
+  });
+});
