97aeb071f5d5222f43e38e82194a8ed079a4ba2d
saikonuru <sairam.konuru@gmail.com>
2025-08-02 14:01:07 +0530

    Simplifing test using helper function renderComponent

--- tests/Components/TermsAndConditions.test.tsx
+++ tests/Components/TermsAndConditions.test.tsx
@@ -3,32 +3,30 @@ import userEvent from "@testing-library/user-event";
 import TermsAndConditions from "../../src/components/TermsAndConditions";
 
 describe("TermsAndConditions", () => {
-  it("should render with correct text and initial state", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />);
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button"),
+    };
+  };
+  it("should render with correct text and initial state", () => {
+    const { heading, checkbox, button } = renderComponent();
+
     expect(heading).toHaveTextContent("Terms & Conditions");
-
-    const checkbox = screen.getByRole("checkbox");
-    expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked();
-
-    const button = screen.getByRole("button"); //("button", { name: /submit/i }); if multiple buttons are there
-    expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
   });
 
   it("should enable the button when the checkbox is checked", async () => {
-    render(<TermsAndConditions />);
-
-    const checkbox = screen.getByRole("checkbox");
-    expect(checkbox).toBeInTheDocument();
+    const { checkbox, button } = renderComponent();
 
     const user = userEvent.setup();
     await user.click(checkbox);
-    expect(checkbox).toBeChecked();
 
-    expect(screen.getByRole("button")).toBeEnabled(); //("button", { name: /submit/i }); if multiple buttons are there
+    expect(checkbox).toBeChecked();
+    expect(button).toBeEnabled();
   });
 });
