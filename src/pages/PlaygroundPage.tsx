ca74e0c26b7e19c4de2baec8ff52b17b8c74b5cf
saikonuru <sairam.konuru@gmail.com>
2025-08-02 15:36:34 +0530

    Tests for onChange events

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,10 +1,11 @@
-import ExpandableText from "../components/ExpandableText";
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
   return (
-    <ExpandableText
-      text="  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eaque maxime possimus vel facilis deleniti perferendis, odio iste nulla soluta molestias velit itaque vitae unde quod in doloremque eveniet at. Saepe aspernatur rerum natus optio culpa obcaecati et, ex eveniet excepturi non est, possimus consectetur similique! Quia doloremque quos harum nemo blanditiis iusto debitis, vel quae veniam suscipit odio necessitatibus magnam ut impedit deserunt modi quo sed consequuntur rem commodi. Numquam labore corrupti corporis unde molestiae iure magnam, fuga inventore ratione accusamus asperiores? Accusamus, illum repellendus in dolores perspiciatis rerum ut dolorum sequi adipisci quibusdam laborum atque numquam molestias? Reiciendis?
-"
+    <SearchBox
+      onChange={(text) => {
+        console.log(text);
+      }}
     />
   );
 };
---
+++ tests/Components/SearchBox.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SearchBox from "../../src/components/SearchBox";
+
+describe("group", () => {
+  const renderComponent = () => {
+    const onChange = vi.fn();
+    render(<SearchBox onChange={onChange} />);
+    return {
+      input: screen.getByPlaceholderText(/search/i),
+      user: userEvent.setup(),
+      onChange,
+    };
+  };
+  it("should render input field for searching", () => {
+    const { input } = renderComponent();
+    expect(input).toBeInTheDocument();
+  });
+
+  it("should call onChange when Enter is pressed", async () => {
+    const { input, user, onChange } = renderComponent();
+    expect(input).toBeInTheDocument();
+
+    const searchTerm = "Search Term";
+    await user.type(input, searchTerm + "{enter}");
+    expect(onChange).toHaveBeenCalledWith(searchTerm);
+  });
+
+  it("should npt call onChange if input field ois empty", async () => {
+    const { input, user, onChange } = renderComponent();
+    expect(input).toBeInTheDocument();
+
+    const searchTerm = "";
+    await user.type(input, searchTerm + "{enter}");
+    expect(onChange).not.toHaveBeenCalledWith(searchTerm);
+  });
+});
